311e79f266e76d26848c2299401bc8262bfc3206
Jose Teodoro da Silva <josets@br.ibm.com>
2017-04-01 12:13:28 -0300

    adding routes to simplify the imports'

--- SampleNodeApp/index.js
+++ SampleNodeApp/index.js
@@ -1,11 +1,6 @@
 import express from 'express';
 import consign from 'consign';
 
-const PORT = 3000;
 const app = express();
 
-app.set('json spaces', 4);
-
-consign().include('models').then('routes').into(app);
-
-app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
+consign().include('models').then('libs/middlewares.js').then('routes').then('libs/boot.js').into(app);
--- SampleNodeApp/routes/tasks.js
+++ SampleNodeApp/routes/tasks.js
@@ -1,10 +1,8 @@
 module.exports = app => {
+  const Tasks = app.models.tasks;
   app.get('/tasks', (req, res) => {
-    res.json({
-      tasks: [
-        {title: 'Shoes'},
-        {title: 'Notebooks'}
-      ]
+    Tasks.findAll({}, (tasks) => {
+      res.json({tasks: tasks});
     });
   });
 };
