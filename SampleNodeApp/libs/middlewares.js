913af14f39d7557e3e93e1f5bc3433c94e377a38
Jose Teodoro da Silva <josets@br.ibm.com>
2017-04-01 12:53:48 -0300

    adding sequelize

--- SampleNodeApp/libs/middlewares.js
+++ SampleNodeApp/libs/middlewares.js
@@ -1,4 +1,4 @@
-module.exports = app => {
+module.exports = function app () {
   app.set('port', 3000);
   app.set('json spaces', 4);
 };
--- SampleNodeApp/routes/tasks.js
+++ SampleNodeApp/routes/tasks.js
@@ -1,8 +1,10 @@
-module.exports = app => {
-  const Tasks = app.models.tasks;
-  app.get('/tasks', (req, res) => {
-    Tasks.findAll({}, (tasks) => {
-      res.json({tasks: tasks});
+module.exports = function app () {
+    const Tasks = app.db.models.Tasks;
+    app.get('/tasks', (req, res) => {
+        Tasks.findAll({}).then(tasks => {
+            res.json({
+                tasks: tasks
+            });
+        });
     });
-  });
 };
